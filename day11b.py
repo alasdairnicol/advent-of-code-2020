3bf97315a6d6f85af889607f3d039a55a4583159
Alasdair Nicol <alasdair@thenicols.net>
2020-12-11 16:15:35 +0000

    Refactor day 11 by adding base class

--- day11b.py
+++ day11b.py
@@ -16,68 +16,26 @@ L.LLLLL.LL
 """
 
 
-class Grid:
+class BaseGrid:
+    occupied_threshold = 0
+
     def __init__(self, grid, width, height):
         # FIXME avoid having to pass width and height around
         self.grid = grid
         self.width = width
         self.height = height
 
-    @staticmethod
-    def from_lines(lines):
+    @classmethod
+    def from_lines(cls, lines):
         grid = {}
         for j, line in enumerate(lines):
             for i, val in enumerate(line):
                 grid[(i, j)] = val
-        return Grid(grid, i + 1, j + 1)
-
-    def neighbours(self, position):
-        deltas = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
-        for dx, dy in deltas:
-            neighbour = (position[0] + dx, position[1] + dy)
-            if neighbour in self.grid:
-                yield neighbour
-
-    def num_occupied_neighbours(self, position):
-        return sum(1 if self.grid[n] == "#" else 0 for n in self.neighbours(position))
-
-    def first_seats(self, position):
-        deltas = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
-        for dx, dy in deltas:
-            x,y = position[0] + dx, position[1] + dy
-            while True:
-                val = self.grid.get((x,y))
-                if val in {"#", "L"}:
-                    # We have found a seat
-                    yield (x,y)
-                    break
-                elif val == None:
-                    # We have reached the end of the grid
-                    break
-                else:
-                    # continue looking
-                    x,y = x+dx, y+dy
-
-    def num_occupied_first_seats(self, position):
-        return sum(1 if self.grid[n] == "#" else 0 for n in self.first_seats(position))
-
+        return cls(grid, i + 1, j + 1)
 
     def display(self):
         for j in range(self.height):
             print("".join(self.grid[(i, j)] for i in range(self.width)))
-
-    def next_grid(self):
-        grid = {}
-        for (x, y), value in self.grid.items():
-            next_value = value  # assume it doesn't change
-            if value == "L" and self.num_occupied_first_seats((x, y)) == 0:
-                next_value = "#"
-            elif value == "#" and self.num_occupied_first_seats((x, y)) >= 5:
-                next_value = "L"
-
-            grid[(x, y)] = next_value
-
-        return Grid(grid, self.width, self.height)
 
     def num_occupied(self):
         return sum(1 if val == "#" else 0 for val in self.grid.values())
@@ -85,10 +43,69 @@ class Grid:
     def __eq__(self, other):
         return self.grid == other.grid
 
+    def next_grid(self):
+        grid = {}
+        for position in self.grid:
+            grid[position] = self.get_next_value(position)
+
+        return type(self)(grid, self.width, self.height)
+
+    def nearby_seats_to_consider(self, position):
+        return iter(())
+
+    def num_occupied_nearby_seats(self, position):
+        return sum(
+            1 if self.grid[n] == "#" else 0
+            for n in self.nearby_seats_to_consider(position)
+        )
+
+    def get_next_value(self, position):
+        value = self.grid[position]  # By default value doesn't change
+        if value == "L" and self.num_occupied_nearby_seats(position) == 0:
+            value = "#"
+        elif (
+            value == "#"
+            and self.num_occupied_nearby_seats(position) >= self.occupied_threshold
+        ):
+            value = "L"
+        return value
+
+
+class NearestNeighbourGrid(BaseGrid):
+    occupied_threshold = 4
+
+    def nearby_seats_to_consider(self, position):
+        deltas = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+        for dx, dy in deltas:
+            neighbour = (position[0] + dx, position[1] + dy)
+            if neighbour in self.grid:
+                yield neighbour
+
+
+class FirstVisibleSeatGrid(BaseGrid):
+    occupied_threshold = 5
+
+    def nearby_seats_to_consider(self, position):
+        deltas = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+        for dx, dy in deltas:
+            x, y = position[0] + dx, position[1] + dy
+            while True:
+                val = self.grid.get((x, y))
+                if val in {"#", "L"}:
+                    # We have found a seat
+                    yield (x, y)
+                    break
+                elif val == None:
+                    # We have reached the end of the grid
+                    break
+                else:
+                    # continue looking
+                    x, y = x + dx, y + dy
+
 
 def main():
     lines = read_input()
-    grid = Grid.from_lines(lines)
+    grid = FirstVisibleSeatGrid.from_lines(lines)
     while True:
         old_grid = grid
         grid = old_grid.next_grid()
