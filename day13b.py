29222208684b7e7874ffb367abe7da0c25c0756f
Alasdair Nicol <alasdair@thenicols.net>
2020-12-13 10:43:19 +0000

    Use functools.reduce in Day 13 part b
    Not sure if this is any clearer, but it was a nice challenge!

--- day13b.py
+++ day13b.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import functools
 import math
 
 TEST_INPUT = """\
@@ -7,18 +8,23 @@ TEST_INPUT = """\
 """
 
 
+def get_time_and_multiplier(acc, new_bus_info):
+    multiplier, time = acc
+    bus, offset = new_bus_info
+    while (time + offset) % bus:
+        time += multiplier
+    multiplier = math.lcm(multiplier, bus)
+    return multiplier, time
+
+
 def main():
     buses = read_input()[1]
     buses_with_offsets = [
         (int(b), offset) for offset, b in enumerate(buses.split(",")) if b != "x"
     ]
-
-    time = 0
-    multiplier = 1
-    for bus, offset in buses_with_offsets:
-        while (time + offset) % bus:
-            time += multiplier
-        multiplier = math.lcm(multiplier, bus)
+    # We get the same answer without the (1, 0) initialiser, but I believe that
+    # it is required to handle the case where the first offset is not zero
+    _, time = functools.reduce(get_time_and_multiplier, buses_with_offsets, (1, 0))
 
     print(time)
 
