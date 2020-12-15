b584a6a12e150c1b21871ba3bafa7ed41eedb176
Alasdair Nicol <alasdair@thenicols.net>
2020-12-15 10:08:25 +0000

    Day 15 simplify code
    Setting previously_spoken = turn means that turn-previously_spoken
    will give 0 as required, and we don't need an if statement to handle
    previously_spoken==0.

--- day15b.py
+++ day15b.py
@@ -8,21 +8,19 @@ REAL_INPUT = "1,20,8,12,0,14"
 
 NUM_TURNS = 30000000
 
+
 def main():
     starting_numbers = [int(x) for x in REAL_INPUT.split(",")]
 
     numbers = {}
     for turn, number in enumerate(starting_numbers, 1):
-        previously_spoken = numbers.get(number, 0)
+        previously_spoken = numbers.get(number, turn)
         numbers[number] = turn
 
     while turn < NUM_TURNS:
-        if previously_spoken == 0:
-            number = 0
-        else:
-            number = turn - previously_spoken
+        number = turn - previously_spoken
         turn += 1
-        previously_spoken = numbers.get(number, 0)
+        previously_spoken = numbers.get(number, turn)
         numbers[number] = turn
 
     print(number)
