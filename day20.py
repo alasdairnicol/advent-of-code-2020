83edc118a5b1235c3bfe97b6465f80abdf6c565b
Alasdair Nicol <alasdair@thenicols.net>
2020-12-21 14:31:18 +0000

    Day 20 part a - actually solve the jigsaw

--- day20.py
+++ day20.py
@@ -115,25 +115,59 @@ Tile 3079:
 """
 
 
+def sort_edge(edge):
+    return min(edge, edge[::-1])
+
+
+def vertical_flip(tile, comment="Flipped vertically"):
+    lines = tile.lines[::-1]
+    return Tile(tile.number, lines, comment=comment)
+
+
+def rotate_clockwise(tile, comment="Rotated 90 deg"):
+    lines = ["".join(i) for i in zip(*tile.lines[::-1])]
+    return Tile(tile.number, lines, comment=comment)
+
+
+def orientations(original_tile):
+    tile = Tile(original_tile.number, original_tile.lines, comment="original")
+    yield tile
+    for deg in [90, 180, 270]:
+        tile = rotate_clockwise(tile, comment=f"Rotated {deg}deg")
+        yield tile
+
+    tile = vertical_flip(original_tile)
+    yield tile
+    for deg in [90, 180, 270]:
+        tile = rotate_clockwise(
+            tile, comment=f"Rotated {deg}deg and flipped vertically"
+        )
+        yield tile
+
+
 class Tile:
-    def __init__(self, number, lines):
+    def __init__(self, number, lines, comment=""):
         self.number = number
         self.lines = lines
+        self.comment = comment
 
     def display(self):
-        print(f"Tile {self.number}")
+        print(f"Tile {self.number} {self.comment}")
         for l in self.lines:
             print("".join(l))
 
+    @property
     def edges(self):
-        edges = [
-            self.lines[0],
-            "".join([l[-1] for l in self.lines]),
-            self.lines[-1],
-            "".join([l[0] for l in self.lines]),
-        ]
-        sorted_edges = [min(e, e[::-1]) for e in edges]
-        return sorted_edges
+        return {
+            "top": self.lines[0],
+            "right": "".join([l[-1] for l in self.lines]),
+            "bottom": self.lines[-1],
+            "left": "".join([l[0] for l in self.lines]),
+        }
+
+    @property
+    def sorted_edges(self):
+        return {sort_edge(v) for v in self.edges.values()}
 
 
 def load_tiles(lines):
@@ -149,13 +183,54 @@ def load_tiles(lines):
     return tiles
 
 
+def find_matching_tile(remaining_tiles, matching_value, side_to_check):
+    for tile in remaining_tiles.values():
+        for tile in orientations(tile):
+            if tile.edges[side_to_check] == matching_value:
+                return tile
+
+
 def main():
     tiles = load_tiles(read_input())
-    edges = itertools.chain.from_iterable(t.edges() for t in tiles)
-    edges_count = collections.Counter(edges)
+    sorted_edges = itertools.chain.from_iterable(t.sorted_edges for t in tiles)
+    edges_count = collections.Counter(sorted_edges)
     outside_edges = {k for k, v in edges_count.items() if v == 1}
-    corner_tiles = [t for t in tiles if len(set(t.edges()) & outside_edges) == 2]
-    print(math.prod([t.number for t in corner_tiles]))
+    corner_tiles = [t for t in tiles if len(set(t.sorted_edges & outside_edges)) == 2]
+
+    jigsaw = {}
+    remaining_tiles = {t.number: t for t in tiles}
+    column, row = 0, 0
+    tile = corner_tiles[0]
+    for tile in orientations(tile):
+        if (
+            sort_edge(tile.edges["top"]) in outside_edges
+            and sort_edge(tile.edges["left"]) in outside_edges
+        ):
+            break
+    jigsaw[(column, row)] = tile
+    del remaining_tiles[tile.number]
+
+    while remaining_tiles:
+        if sort_edge(tile.edges["bottom"]) in outside_edges:
+            row = 0
+            previous_tile = jigsaw[(column, 0)]
+            column += 1
+            matching_value = previous_tile.edges["right"]
+            side_to_check = "left"
+        else:
+            previous_tile = jigsaw[(column, row)]
+            row += 1
+            matching_value = previous_tile.edges["bottom"]
+            side_to_check = "top"
+
+        tile = find_matching_tile(remaining_tiles, matching_value, side_to_check)
+        jigsaw[(column, row)] = tile
+        del remaining_tiles[tile.number]
+
+    width = max(x for (x, y) in jigsaw) + 1
+    height = max(y for (x, y) in jigsaw) + 1
+
+    print(math.prod(t.number for t in corner_tiles))
 
 
 def read_test_input():
